924078d44fe6d9e19306e541dad85fbb89228850
shanshanyang <office.ss.nyc@gmail.com>
2016-04-01 11:27:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Green Card Priority Date checker Command line interface
+
+> check your priority date in Terminal
+
+## Usage
+
+clone or fork this repo and run `node index.js`
+
+![Imgur](http://i.imgur.com/zXCrQrN.gif)
---
+++ index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const inquirer = require('inquirer'),
+priorityDate = require('priority-date').default,
+_ = require('lodash/object'),
+currentMonth = new Date().getUTCMonth() + 1,
+nextMonth = currentMonth === 12 ? 1 : currentMonth + 1,
+year = new Date().getUTCFullYear();
+
+const questions = [
+  {
+    type: 'checkbox',
+    message: 'Look up cut-off dates for:',
+    name: 'Month',
+    choices: [
+      {
+        name: `${currentMonth}-${year}`
+      },
+      {
+        name: `${nextMonth}-${year}`
+      }
+    ],
+    validate: function(answer) {
+      if (answer.length < 1) {
+        return "Please choose a month"
+      }
+      return true;
+    }
+  },
+  {
+    type: 'list',
+    name: 'Category',
+    message: 'What is your Immigration Category?',
+    choices: [
+      'familySponsered',
+      'employmentBased'
+    ]
+  }
+];
+
+inquirer.prompt( questions, function( answers ) {
+  let moreQuestions = [
+    {
+      type: 'list',
+      name: 'Country',
+      message: 'What is your country of birth?',
+      choices: [
+        'chinaMLB',
+        'philippines',
+        'mexico',
+        'india',
+        'allExcept'
+      ]
+    }
+  ];
+
+  if (answers.Category === 'employmentBased') {
+    moreQuestions = moreQuestions.concat({
+      type: 'list',
+      name: 'Type',
+      message: 'What is your employment based category?',
+      choices: [
+        'first',
+        'second',
+        'third',
+        'fourth',
+        'fifth',
+        'fifthb',
+        'otherWorkers',
+        'certainReligiousWorkers'
+      ]
+    });
+  }
+
+  if (answers.Category === 'familySponsered') {
+    moreQuestions = moreQuestions.concat({
+      type: 'list',
+      name: 'Type',
+      message: 'What is your family based category?',
+      choices: [
+        'F1',
+        'F2a',
+        'F2b',
+        'F3',
+        'F4'
+      ]
+    });
+  }
+
+  inquirer.prompt(moreQuestions, function(moreanswers) {
+
+    const info = _.merge(answers, moreanswers),
+          arr = [].concat(info['Month'][0]).concat(info['Category']).concat(info['Type']).concat(info['Country']),
+          arr_1 = [].concat(info['Month'][1]).concat(info['Category']).concat(info['Type']).concat(info['Country']);
+
+    console.log("\nYour info:", info);
+
+    priorityDate().then(function(result) {
+      console.log("\nYour priority date for currentMonth: ", _.result(result, arr.join('.')));
+
+      if (info['Month'].length > 1){
+        console.log("\nYour priority date for nextMonth: ", _.result(result, arr_1.join('.')));
+      }
+    });
+  });
+});
